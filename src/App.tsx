7cb9cc0f0a181d61cbbbd881cd35840c5f7aa7c1
sanskarOH <sanskaryeeha@gmail.com>
2026-02-06 14:29:35 +0530

    added github calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { MainBanner } from "./components/sections/MainBanner"
+import Hero from "./sections/Hero"
 function App() {
  
   return (
     <>
-      <div className="min-h-screen items-center flex flex-col p-10">
-        <h1>Sanskar Diwedi</h1>
-        <Avatar className="m-3">
-        <AvatarImage
-            src="https://github.com/shadcn.png"
-            alt="shadcan"
-            className=""
-        ></AvatarImage>
-        </Avatar>
-        <MainBanner />
-       
-      </div>
+    <div className=" min-h-screen w-full flex justify-center">
+      <Hero />
+    </div>
+      
     </>
   )
 }
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,16 @@
+import { GitHubCalendar } from "react-github-calendar"
+
+export default function Hero(){
+    return(
+        <section id="hero" className="py-24">
+            <div className="max-w-6xl mx-auto px-4">
+                {/*content*/}
+                <div className="wrapper border-gray-400 border-2 rounded-2xl">
+                    <div className="p-5">
+                    <GitHubCalendar className="" colorScheme="dark" username="sanskarOH" />
+                </div>
+                </div>
+            </div>
+        </section>
+    )
+}
