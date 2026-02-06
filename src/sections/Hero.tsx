3abcb26f2eea554501aeddb4812348b6094a3bc5
sanskarOH <sanskaryeeha@gmail.com>
2026-02-06 20:51:32 +0530

    added morphing animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,17 @@
-import SkillTerminal from "@/components/SkillTerminal"
-import { BlurFade } from "@/components/ui/blur-fade"
+import SkillTerminal from "@/components/SkillTerminal";
+import { BlurFade } from "@/components/ui/blur-fade";
+import { MorphingText } from "@/components/ui/morphing-text";
 
 export default function Hero() {
+  const text = ["Developer", "Founder", "Freelancer", "Meow <3"];
   return (
     <section className="py-20">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-
         {/* Heading */}
         <BlurFade delay={0.25} inView>
-            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight mb-12">
-          Sanskar Diwedi 👋
-        </h1>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight mb-12">
+            Sanskar Diwedi 👋
+          </h1>
         </BlurFade>
 
         {/* BENTO GRID */}
@@ -25,16 +26,18 @@ export default function Hero() {
         >
           {/* BIG TERMINAL TILE */}
           <div
-            className="
+            className="flex
               md:col-span-2
               md:row-span-2
               rounded-2xl
               border
               bg-background
               p-4
+              justify-center
+              items-center
             "
           >
-            
+            <MorphingText texts={text} />
           </div>
 
           {/* SMALL TILE 1 */}
@@ -42,7 +45,6 @@ export default function Hero() {
             <SkillTerminal />
           </div>
 
-          {/* SMALL TILE 2 */}
           <div className="rounded-2xl border p-6">
             <h3 className="font-semibold">Currently Learning</h3>
             <p className="text-sm text-muted-foreground mt-2">
@@ -50,7 +52,6 @@ export default function Hero() {
             </p>
           </div>
 
-          {/* WIDE TILE */}
           <div className="md:col-span-3 rounded-2xl border p-6">
             <p className="text-sm text-muted-foreground">
               I enjoy building scalable backend systems and clean developer
@@ -58,8 +59,7 @@ export default function Hero() {
             </p>
           </div>
         </div>
-
       </div>
     </section>
-  )
+  );
 }
